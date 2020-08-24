8bd9fdf397f95951332ed9a96d1b43ee05f9df5c
TruongThanhHai28 <truongthanhhai283@gmail.com>
2020-08-25 00:12:26 +0700

    Edit Post Handler

--- ASP.NET-Core_Master/ASP.NET-Core_Master/Pages/BookList/Edit.cshtml.cs
+++ ASP.NET-Core_Master/ASP.NET-Core_Master/Pages/BookList/Edit.cshtml.cs
@@ -17,10 +17,28 @@ namespace ASP.NET_Core_Master.Pages.BookList
             _db = db;
         }
 
+        [BindProperty]
         public Book Book { get; set; }
         public async Task OnGet(int? id)
         {
             Book =await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
         }
     }
 }

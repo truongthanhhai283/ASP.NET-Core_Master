c4b70e5d67def0a3e7edff03a4ba4a6c0d04111d
TruongThanhHai28 <truongthanhhai283@gmail.com>
2020-08-24 23:35:03 +0700

    Edit Book Get Handler

---
+++ ASP.NET-Core_Master/ASP.NET-Core_Master/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Core_Master.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Master.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Book Book { get; set; }
+        public async Task OnGet(int? id)
+        {
+            Book =await _db.Book.FindAsync(id);
+        }
+    }
+}
